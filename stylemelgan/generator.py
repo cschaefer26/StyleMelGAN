b70955d54060cfea4a2bb6f94fa0cc2ba251ade6
cschaefer26 <c.schaefer.home@gmail.com>
2021-10-01 12:43:05 +0200

    Autoenc.

--- stylemelgan/generator.py
+++ stylemelgan/generator.py
@@ -57,13 +57,13 @@ class Autoencoder(Module):
     def __init__(self, n_mels: int = 80) -> None:
         super().__init__()
         self.convs = Sequential(
-            WNConv1d(n_mels, 64, 3, padding=1),
+            WNConv1d(n_mels, 128, 3, padding=1),
             LeakyReLU(0.2),
             Dropout(0.5),
-            WNConv1d(64, 64, 3, padding=1),
+            WNConv1d(128, 128, 3, padding=1),
             LeakyReLU(0.2),
             Dropout(0.5),
-            WNConv1d(64, n_mels, 3, padding=1),
+            WNConv1d(128, n_mels, 3, padding=1),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
