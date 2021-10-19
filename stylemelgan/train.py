be485da3b2e77ee6c617918fe99e3d9fefd63d35
cschaefer26 <c.schaefer.home@gmail.com>
2021-10-19 12:41:57 +0200

    adjusted seg len.

--- stylemelgan/train.py
+++ stylemelgan/train.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
                 for i, val_data in enumerate(val_dataset):
                     val_mel = val_data['mel'].to(device)
                     val_mel = val_mel.unsqueeze(0)
-                    wav_fake = g_model.inference(val_mel, pad_steps=80).squeeze().cpu().numpy()
+                    wav_fake = g_model.inference(val_mel).squeeze().cpu().numpy()
                     wav_real = val_data['wav'].detach().squeeze().cpu().numpy()
                     wav_f = torch.tensor(wav_fake).unsqueeze(0).to(device)
                     wav_r = torch.tensor(wav_real).unsqueeze(0).to(device)
