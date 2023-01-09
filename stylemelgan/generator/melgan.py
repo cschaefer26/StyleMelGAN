1c1be3f7b40306a76da57ba8028d391b2b035f30
cschafer26 <c.schaefer.home@gmail.com>
2023-01-09 20:00:30 +0100

    res

--- stylemelgan/generator/melgan.py
+++ stylemelgan/generator/melgan.py
@@ -15,76 +15,187 @@ MAX_WAV_VALUE = 32768.0
 
 
 class ResStack(nn.Module):
-    def __init__(self, channel, num_layers=4):
+    def __init__(self, channel, dilation=1):
         super(ResStack, self).__init__()
 
-        self.blocks = nn.ModuleList([
-            nn.Sequential(
-                nn.LeakyReLU(0.2),
-                nn.ReflectionPad1d(3**i),
-                nn.utils.weight_norm(nn.Conv1d(channel, 2*channel, kernel_size=3, dilation=3**i)),
-                nn.LeakyReLU(0.2),
-                nn.ReflectionPad1d(1),
-                nn.utils.weight_norm(nn.Conv1d(2*channel, channel, kernel_size=3)),
-            )
-            for i in range(num_layers)
-        ])
+        self.block = nn.Sequential(
+            nn.LeakyReLU(0.2),
+            nn.ReflectionPad1d(dilation),
+            nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=3, dilation=dilation)),
+            nn.LeakyReLU(0.2),
+            nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=1)),
+        )
 
-        self.shortcuts = nn.ModuleList([
-            nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=1))
-            for i in range(num_layers)
-        ])
+
+        self.shortcut = nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=1))
+
 
     def forward(self, x):
-        for block, shortcut in zip(self.blocks, self.shortcuts):
-            x = shortcut(x) + block(x)
-        return x
+        return self.shortcut(x) + self.block(x)
 
     def remove_weight_norm(self):
-        for block, shortcut in zip(self.blocks, self.shortcuts):
-            nn.utils.remove_weight_norm(block[2])
-            nn.utils.remove_weight_norm(block[4])
-            nn.utils.remove_weight_norm(shortcut)
+        nn.utils.remove_weight_norm(self.block[2])
+        nn.utils.remove_weight_norm(self.block[4])
+        nn.utils.remove_weight_norm(self.shortcut)
+        # def _remove_weight_norm(m):
+        #     try:
+        #         torch.nn.utils.remove_weight_norm(m)
+        #     except ValueError:  # this module didn't have weight norm
+        #         return
+        #
+        # self.apply(_remove_weight_norm)
 
 
 class Generator(nn.Module):
-    def __init__(self, mel_channel):
-        super(Generator, self).__init__()
+    def __init__(self, mel_channel, ratios=[4, 4, 2, 2, 2, 2], mult=256, out_band=1):
+        super().__init__()
         self.mel_channel = mel_channel
 
-        self.generator = nn.Sequential(
+        self.start = nn.Sequential(
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(mel_channel, 1024, kernel_size=7, stride=1)),
-
+            nn.utils.weight_norm(nn.Conv1d(mel_channel, mult * 2, kernel_size=7, stride=1))
+        )
+        r = ratios[0]
+        self.upsample_1 = nn.Sequential(
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(1024, 256, kernel_size=16, stride=8, padding=4)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(mult * 2, mult,
+                                                    kernel_size=r * 2, stride=r,
+                                                    padding=r // 2 + r % 2,
+                                                    output_padding=r % 2)
+                                 )
+        )
+        self.res_stack_1 = nn.Sequential(*[ResStack(mult, dilation=3 ** j) for j in range(4)])
 
-            ResStack(256, num_layers=5),
-
+        r = ratios[1]
+        mult = mult // 2
+        self.upsample_2 = nn.Sequential(
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(256, 64, kernel_size=16, stride=8, padding=4)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(mult * 2, mult,
+                                                    kernel_size=r * 2, stride=r,
+                                                    padding=r // 2 + r % 2,
+                                                    output_padding=r % 2)
+                                 )
+        )
+        self.res_stack_2 = nn.Sequential(*[ResStack(mult, dilation=3 ** j) for j in range(5)])
 
-            ResStack(64, num_layers=7),
-
+        r = ratios[2]
+        mult = mult // 2
+        self.upsample_3 = nn.Sequential(
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(64, 32, kernel_size=4, stride=2, padding=1)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(mult * 2, mult,
+                                                    kernel_size=r * 2, stride=r,
+                                                    padding=r // 2 + r % 2,
+                                                    output_padding=r % 2)
+                                 )
+        )
 
-            ResStack(32, num_layers=8),
+        self.skip_upsample_1 = nn.utils.weight_norm(nn.ConvTranspose1d(mel_channel, mult,
+                                                                       kernel_size=64, stride=32,
+                                                                       padding=16,
+                                                                       output_padding=0)
+                                                    )
+        self.res_stack_3 = nn.Sequential(*[ResStack(mult, dilation=3 ** j) for j in range(6)])
 
+
+
+        r = ratios[3]
+        mult = mult // 2
+        self.upsample_4 = nn.Sequential(
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(32, 16, kernel_size=4, stride=2, padding=1)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(mult * 2, mult,
+                                                    kernel_size=r * 2, stride=r,
+                                                    padding=r // 2 + r % 2,
+                                                    output_padding=r % 2)
+                                 )
+        )
 
-            ResStack(16, num_layers=9),
+        self.skip_upsample_2 = nn.utils.weight_norm(nn.ConvTranspose1d(mel_channel, mult,
+                                                                       kernel_size=128, stride=64,
+                                                                       padding=32,
+                                                                       output_padding=0)
+                                                    )
+        self.res_stack_4 = nn.Sequential(*[ResStack(mult, dilation=3 ** j) for j in range(7)])
 
+
+        r = ratios[4]
+        mult = mult // 2
+        self.upsample_5 = nn.Sequential(
+            nn.LeakyReLU(0.2),
+            nn.utils.weight_norm(nn.ConvTranspose1d(mult * 2, mult,
+                                                    kernel_size=r * 2, stride=r,
+                                                    padding=r // 2 + r % 2,
+                                                    output_padding=r % 2)
+                                 )
+        )
+
+        self.skip_upsample_3 = nn.utils.weight_norm(nn.ConvTranspose1d(mel_channel, mult,
+                                                                       kernel_size=256, stride=128,
+                                                                       padding=64,
+                                                                       output_padding=0)
+                                                    )
+        self.res_stack_5 = nn.Sequential(*[ResStack(mult, dilation=3 ** j) for j in range(8)])
+
+
+        r = ratios[5]
+        mult = mult // 2
+        self.upsample_6 = nn.Sequential(
+            nn.LeakyReLU(0.2),
+            nn.utils.weight_norm(nn.ConvTranspose1d(mult * 2, mult,
+                                                    kernel_size=r * 2, stride=r,
+                                                    padding=r // 2 + r % 2,
+                                                    output_padding=r % 2)
+                                 )
+        )
+
+        self.skip_upsample_4 = nn.utils.weight_norm(nn.ConvTranspose1d(mel_channel, mult,
+                                                                       kernel_size=512, stride=256,
+                                                                       padding=128,
+                                                                       output_padding=0)
+                                                    )
+        self.res_stack_6 = nn.Sequential(*[ResStack(mult, dilation=3 ** j) for j in range(9)])
+
+        self.out = nn.Sequential(
             nn.LeakyReLU(0.2),
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(16, 1, kernel_size=7, stride=1)),
+            nn.utils.weight_norm(nn.Conv1d(mult, out_band, kernel_size=7, stride=1)),
             nn.Tanh(),
         )
 
+
     def forward(self, mel):
-        mel = (mel + 5.0) / 5.0 # roughly normalize spectrogram
-        return self.generator(mel)
+        mel = (mel + 5.0) / 5.0  # roughly normalize spectrogram
+        # Mel Shape [B, num_mels, T] -> torch.Size([3, 80, 10])
+        x = self.start(mel)  # [B, dim*2, T] -> torch.Size([3, 512, 10])
+
+        x = self.upsample_1(x)
+        x = self.res_stack_1(x)  # [B, dim, T*4] -> torch.Size([3, 256, 40])
+
+        x = self.upsample_2(x)
+        x = self.res_stack_2(x)  # [B, dim/2, T*16] -> torch.Size([3, 128, 160])
+        # out1 = self.sub_out_1(x)  # [B, 1, T*16] -> torch.Size([3, 1, 160])
+
+        x = self.upsample_3(x)
+        x = x + self.skip_upsample_1(mel)
+        x = self.res_stack_3(x)  # [B, dim/4, T*32] -> torch.Size([3, 64, 320])
+        # out2 = self.sub_out_2(x)  # [B, 1, T*32] -> torch.Size([3, 1, 320])
+
+        x = self.upsample_4(x)
+        x = x + self.skip_upsample_2(mel)
+        x = self.res_stack_4(x)  # [B, dim/8, T*64] -> torch.Size([3, 32, 640])
+        # out3 = self.sub_out_3(x)  # [B, 1, T*64] -> torch.Size([3, 1, 640])
+
+        x = self.upsample_5(x)
+        x = x + self.skip_upsample_3(mel)
+        x = self.res_stack_5(x)  # [B, dim/16, T*128] -> torch.Size([3, 16, 1280])
+        # out4 = self.sub_out_4(x)  # [B, 1, T*128] -> torch.Size([3, 1, 1280])
+
+        x = self.upsample_6(x)
+        x = x + self.skip_upsample_4(mel)
+        x = self.res_stack_6(x)  # [B, dim/32, T*256] -> torch.Size([3, 8, 2560])
+
+        out = self.out(x)  # [B, 1, T*256] -> torch.Size([3, 1, 2560])
+
+        return out #out1, out2, out3, out4, out
 
     def eval(self, inference=False):
         super(Generator, self).eval()
@@ -93,36 +204,44 @@ class Generator(nn.Module):
         if inference:
             self.remove_weight_norm()
 
+    # def remove_weight_norm(self):
+    #     for idx, layer in enumerate(self.generator):
+    #         if len(layer.state_dict()) != 0:
+    #             try:
+    #                 nn.utils.remove_weight_norm(layer)
+    #             except:
+    #                 layer.remove_weight_norm()
+
     def remove_weight_norm(self):
-        for idx, layer in enumerate(self.generator):
-            if len(layer.state_dict()) != 0:
-                try:
-                    nn.utils.remove_weight_norm(layer)
-                except:
-                    layer.remove_weight_norm()
+        """Remove weight normalization module from all of the layers."""
 
-    def inference(self,
-                  mel: torch.Tensor,
-                  pad_steps: int = 10) -> torch.Tensor:
-        with torch.no_grad():
-            pad = torch.full((1, 80, pad_steps), -11.5129).to(mel.device)
-            mel = torch.cat((mel, pad), dim=2)
-            audio = self.forward(mel).squeeze()
-            audio = audio[:-(256 * pad_steps)]
-        return audio
+        def _remove_weight_norm(m):
+            try:
+                torch.nn.utils.remove_weight_norm(m)
+            except ValueError:  # this module didn't have weight norm
+                return
 
-    @classmethod
-    def from_config(cls, config: Dict[str, Any]) -> 'Generator':
-        return Generator(mel_channels=config['audio']['n_mels'],
-                         **config['model'])
+        self.apply(_remove_weight_norm)
 
-    @classmethod
-    def from_checkpoint(cls, file: str) -> 'Generator':
-        checkpoint = torch.load(file, map_location=torch.device('cpu'))
-        config = checkpoint['config']
-        model = Generator.from_config(config)
-        model.load_state_dict(config['g_model'])
-        return model
+    def apply_weight_norm(self):
+        """Apply weight normalization module from all of the layers."""
+
+        def _apply_weight_norm(m):
+            if isinstance(m, torch.nn.Conv1d) or isinstance(m, torch.nn.ConvTranspose1d):
+                torch.nn.utils.weight_norm(m)
+
+        self.apply(_apply_weight_norm)
+
+
+    def inference(self, mel):
+        hop_length = 256
+        # pad input mel with zeros to cut artifact
+        # see https://github.com/seungwonpark/melgan/issues/8
+        zero = torch.full((1, self.mel_channel, 10), -11.5129).to(mel.device)
+        mel = torch.cat((mel, zero), dim=2)
+
+        audio = self.forward(mel)
+        return
 
 
 if __name__ == '__main__':
