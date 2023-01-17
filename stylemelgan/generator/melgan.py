6d88ca68752561b6740f0d6f2e21409766fef721
cschafer26 <c.schaefer.home@gmail.com>
2023-01-17 10:05:22 +0100

    testing.

--- stylemelgan/generator/melgan.py
+++ stylemelgan/generator/melgan.py
@@ -52,32 +52,32 @@ class Generator(nn.Module):
         self.mel_channel = mel_channel
 
         self.generator = nn.Sequential(
-            nn.ReflectionPad1d(7),
-            nn.utils.weight_norm(nn.Conv1d(mel_channel, 256, kernel_size=15, stride=1)),
+            nn.ReflectionPad1d(3),
+            nn.utils.weight_norm(nn.Conv1d(mel_channel, 512, kernel_size=7, stride=1)),
+
+            nn.LeakyReLU(0.2),
+            nn.utils.weight_norm(nn.ConvTranspose1d(512, 256, kernel_size=16, stride=8, padding=4)),
+
+            ResStack(256, num_layers=5),
 
             nn.LeakyReLU(0.2),
             nn.utils.weight_norm(nn.ConvTranspose1d(256, 128, kernel_size=16, stride=8, padding=4)),
 
-            ResStack(128, num_layers=5),
+            ResStack(128, num_layers=7),
 
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(128, 64, kernel_size=16, stride=8, padding=4)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(128, 64, kernel_size=4, stride=2, padding=1)),
 
-            ResStack(64, num_layers=7),
+            ResStack(64, num_layers=8),
 
             nn.LeakyReLU(0.2),
             nn.utils.weight_norm(nn.ConvTranspose1d(64, 32, kernel_size=4, stride=2, padding=1)),
 
-            ResStack(32, num_layers=8),
-
-            nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(32, 16, kernel_size=4, stride=2, padding=1)),
-
-            ResStack(16, num_layers=9),
+            ResStack(32, num_layers=9),
 
             nn.LeakyReLU(0.2),
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(16, 1, kernel_size=7, stride=1)),
+            nn.utils.weight_norm(nn.Conv1d(32, 1, kernel_size=7, stride=1)),
             nn.Tanh(),
         )
 
@@ -100,28 +100,21 @@ class Generator(nn.Module):
                 except:
                     layer.remove_weight_norm()
 
-    def inference(self,
-                  mel: torch.Tensor,
-                  pad_steps: int = 10) -> torch.Tensor:
-        with torch.no_grad():
-            pad = torch.full((1, 80, pad_steps), -11.5129).to(mel.device)
-            mel = torch.cat((mel, pad), dim=2)
-            audio = self.forward(mel).squeeze()
-            audio = audio[:-(256 * pad_steps)]
+    def inference(self, mel):
+        hop_length = 256
+        # pad input mel with zeros to cut artifact
+        # see https://github.com/seungwonpark/melgan/issues/8
+        zero = torch.full((1, self.mel_channel, 10), -11.5129).to(mel.device)
+        mel = torch.cat((mel, zero), dim=2)
+
+        audio = self.forward(mel)
+        audio = audio.squeeze() # collapse all dimension except time axis
+        audio = audio[:-(hop_length*10)]
+        audio = MAX_WAV_VALUE * audio
+        audio = audio.clamp(min=-MAX_WAV_VALUE, max=MAX_WAV_VALUE-1)
+        audio = audio.short()
+
         return audio
-
-    @classmethod
-    def from_config(cls, config: Dict[str, Any]) -> 'Generator':
-        return Generator(mel_channels=config['audio']['n_mels'],
-                               **config['model'])
-
-    @classmethod
-    def from_checkpoint(cls, file: str) -> 'Generator':
-        checkpoint = torch.load(file, map_location=torch.device('cpu'))
-        config = checkpoint['config']
-        model = Generator.from_config(config)
-        model.load_state_dict(config['g_model'])
-        return model
 
 
 if __name__ == '__main__':
