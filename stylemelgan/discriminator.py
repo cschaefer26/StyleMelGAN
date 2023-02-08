ba7aa3ff6c98180a1a73e5bcd9886708dc38609a
cschafer26 <c.schaefer.home@gmail.com>
2023-02-08 07:12:21 +0100

    pitch loss.

--- stylemelgan/discriminator.py
+++ stylemelgan/discriminator.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn as nn
 from torch.nn import Sequential, LeakyReLU
-
+from torch.nn.utils import weight_norm, spectral_norm
+import torch.nn.functional as F
 from stylemelgan.common import WNConv1d
+from torch.nn import Conv1d, ConvTranspose1d, AvgPool1d, Conv2d, Linear, GRU
 
+LRELU_SLOPE = 0.1
 
 class Identity(nn.Module):
     def __init__(self):
@@ -12,6 +15,21 @@ class Identity(nn.Module):
     def forward(self, x):
         return x
 
+
+def init_weights(m, mean=0.0, std=0.01):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        m.weight.data.normal_(mean, std)
+
+
+def apply_weight_norm(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        weight_norm(m)
+
+
+def get_padding(kernel_size, dilation=1):
+    return int((kernel_size*dilation - dilation)/2)
 
 
 class PitchPredictor(nn.Module):
@@ -119,8 +137,63 @@ class MultiScaleDiscriminator(nn.Module):
 
 
 
+class DiscriminatorP(torch.nn.Module):
+    def __init__(self, period, kernel_size=5, stride=3, use_spectral_norm=False):
+        super(DiscriminatorP, self).__init__()
+        self.period = period
+        norm_f = weight_norm if use_spectral_norm == False else spectral_norm
+        self.convs = nn.ModuleList([
+            norm_f(Conv2d(1, 32, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
+            norm_f(Conv2d(32, 128, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
+            norm_f(Conv2d(128, 512, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
+            norm_f(Conv2d(512, 1024, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
+            norm_f(Conv2d(1024, 1024, (kernel_size, 1), 1, padding=(2, 0))),
+        ])
+        self.conv_post = norm_f(Conv2d(1024, 1, (3, 1), 1, padding=(1, 0)))
+
+    def forward(self, x):
+        fmap = []
+
+        # 1d to 2d
+        b, c, t = x.shape
+        if t % self.period != 0: # pad first
+            n_pad = self.period - (t % self.period)
+            x = F.pad(x, (0, n_pad), "reflect")
+            t = t + n_pad
+        x = x.view(b, c, t // self.period, self.period)
+
+        for l in self.convs:
+            x = l(x)
+            x = F.leaky_relu(x, LRELU_SLOPE)
+            fmap.append(x)
+        x = self.conv_post(x)
+        fmap.append(x)
+        x = torch.flatten(x, 1, -1)
+
+        return fmap, x
+
+
+class MultiPeriodDiscriminator(torch.nn.Module):
+    def __init__(self):
+        super(MultiPeriodDiscriminator, self).__init__()
+        self.discriminators = nn.ModuleList([
+            DiscriminatorP(2),
+            DiscriminatorP(3),
+            DiscriminatorP(5),
+            DiscriminatorP(7),
+            DiscriminatorP(11),
+        ])
+
+    def forward(self, y):
+        ret = list()
+        for i, d in enumerate(self.discriminators):
+            ret.append(d(x))
+
+        return ret
+
+
 if __name__ == '__main__':
-    model = Discriminator()
+    model = MultiPeriodDiscriminator()
 
     x = torch.randn(3, 1, 22050)
     print(x.shape)
