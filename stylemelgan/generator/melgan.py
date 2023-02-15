87b6ba6882f930cf7c115f473df0247997d0e44c
cschafer26 <c.schaefer.home@gmail.com>
2023-02-15 09:04:20 +0100

    fix.

--- stylemelgan/generator/melgan.py
+++ stylemelgan/generator/melgan.py
@@ -53,31 +53,31 @@ class Generator(nn.Module):
 
         self.generator = nn.Sequential(
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(mel_channel, 512, kernel_size=7, stride=1)),
+            nn.utils.weight_norm(nn.Conv1d(mel_channel, 512 + 256, kernel_size=7, stride=1)),
 
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(512, 256, kernel_size=16, stride=8, padding=4)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(512 + 256, 256 + 128, kernel_size=16, stride=8, padding=4)),
 
-            ResStack(256, num_layers=5),
-
-            nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(256, 128, kernel_size=16, stride=8, padding=4)),
-
-            ResStack(128, num_layers=7),
+            ResStack(256 + 128, num_layers=4),
 
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(128, 64, kernel_size=4, stride=2, padding=1)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(256 + 128, 128 + 64, kernel_size=16, stride=8, padding=4)),
 
-            ResStack(64, num_layers=8),
+            ResStack(128 + 64, num_layers=4),
 
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(64, 32, kernel_size=4, stride=2, padding=1)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(128 + 64, 64 + 32, kernel_size=4, stride=2, padding=1)),
 
-            ResStack(32, num_layers=9),
+            ResStack(64 + 32, num_layers=5),
+
+            nn.LeakyReLU(0.2),
+            nn.utils.weight_norm(nn.ConvTranspose1d(64 + 32, 32 + 16, kernel_size=4, stride=2, padding=1)),
+
+            ResStack(32 + 16, num_layers=6),
 
             nn.LeakyReLU(0.2),
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(32, 1, kernel_size=7, stride=1)),
+            nn.utils.weight_norm(nn.Conv1d(32 + 16, 1, kernel_size=7, stride=1)),
             nn.Tanh(),
         )
 
