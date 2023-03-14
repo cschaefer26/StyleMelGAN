73cc10ae2a1cb434ec13b3fecb5d676667719071
cschafer26 <c.schaefer.home@gmail.com>
2023-03-14 18:36:09 +0100

    Rnn

--- stylemelgan/generator/melgan.py
+++ stylemelgan/generator/melgan.py
@@ -28,8 +28,8 @@ class PositionalEncoding(torch.nn.Module):
 
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        pe[:, 0::2] = torch.sin(position * self.scale_1)
-        pe[:, 1::2] = torch.cos(position * self.scale_2)
+        pe[:, 0::2] = torch.sin(position * 0.0001)
+        pe[:, 1::2] = torch.cos(position * 0.0001)
         pe = pe.unsqueeze(0).transpose(0, 1)
         self.register_buffer('pe', pe)
 
