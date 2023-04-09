461cc592991ff7b54092774c4c87bbb319d6ec67
cschafer26 <c.schaefer.home@gmail.com>
2023-04-09 10:32:28 +0200

    Double res.

--- stylemelgan/generator/melgan.py
+++ stylemelgan/generator/melgan.py
@@ -55,28 +55,28 @@ class Generator(nn.Module):
 
         self.generator = nn.Sequential(
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(mel_channel, 1024, kernel_size=7, stride=1)),
+            nn.utils.weight_norm(nn.Conv1d(mel_channel, 512, kernel_size=7, stride=1)),
 
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(1024, 512, kernel_size=16, stride=8, padding=4)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(512, 256, kernel_size=16, stride=8, padding=4)),
 
-            ResStack(512, num_layers=5),
-
-            nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(512, 256, kernel_size=8, stride=4, padding=2)),
-
-            ResStack(256, num_layers=6),
+            ResStack(256, num_layers=5),
 
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(256, 128, kernel_size=4, stride=2, padding=1)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(256, 128, kernel_size=16, stride=8, padding=4)),
 
             ResStack(128, num_layers=7),
 
             nn.LeakyReLU(0.2),
+            nn.utils.weight_norm(nn.ConvTranspose1d(128, 64, kernel_size=4, stride=2, padding=1)),
+
+            ResStack(64, num_layers=8),
+
+            nn.LeakyReLU(0.2),
         )
 
-        self.post_n_fft = 16
-        self.conv_post = weight_norm(Conv1d(128, self.post_n_fft + 2, 7, 1, padding=3))
+        self.post_n_fft = 8
+        self.conv_post = weight_norm(Conv1d(64, self.post_n_fft + 2, 7, 1, padding=3))
         self.reflection_pad = torch.nn.ReflectionPad1d((1, 0))
 
     def forward(self, mel):
@@ -129,7 +129,7 @@ class Generator(nn.Module):
 if __name__ == '__main__':
     import time
     config = read_config('../configs/melgan_config.yaml')
-    torch_stft = TorchSTFT(filter_length=16, hop_length=4, win_length=16)
+    torch_stft = TorchSTFT(filter_length=8, hop_length=2, win_length=8)
     model = Generator(80)
     x = torch.randn(3, 80, 1000)
     start = time.time()
