e0991e779c3216b23009622c64466a644ae7772d
cschafer26 <c.schaefer.home@gmail.com>
2023-04-16 14:37:03 +0200

    Snake.

--- stylemelgan/generator/melgan.py
+++ stylemelgan/generator/melgan.py
@@ -1,7 +1,8 @@
 from typing import Tuple, Dict, Any
 
 import torch
-from torch.nn import Module, ModuleList, Sequential, LeakyReLU, Tanh
+from torch import sin
+from torch.nn import Module, ModuleList, Sequential, LeakyReLU, Tanh, Parameter
 
 from stylemelgan.common import WNConv1d, WNConvTranspose1d
 from stylemelgan.utils import read_config
@@ -12,7 +13,65 @@ import numpy as np
 
 MAX_WAV_VALUE = 32768.0
 
+class SnakeBeta(nn.Module):
+    '''
+    A modified Snake function which uses separate parameters for the magnitude of the periodic components
+    Shape:
+        - Input: (B, C, T)
+        - Output: (B, C, T), same shape as the input
+    Parameters:
+        - alpha - trainable parameter that controls frequency
+        - beta - trainable parameter that controls magnitude
+    References:
+        - This activation function is a modified version based on this paper by Liu Ziyin, Tilman Hartwig, Masahito Ueda:
+        https://arxiv.org/abs/2006.08195
+    Examples:
+        >>> a1 = snakebeta(256)
+        >>> x = torch.randn(256)
+        >>> x = a1(x)
+    '''
+    def __init__(self, in_features, alpha=1.0, alpha_trainable=True, alpha_logscale=False):
+        '''
+        Initialization.
+        INPUT:
+            - in_features: shape of the input
+            - alpha - trainable parameter that controls frequency
+            - beta - trainable parameter that controls magnitude
+            alpha is initialized to 1 by default, higher values = higher-frequency.
+            beta is initialized to 1 by default, higher values = higher-magnitude.
+            alpha will be trained along with the rest of your model.
+        '''
+        super(SnakeBeta, self).__init__()
+        self.in_features = in_features
 
+        # initialize alpha
+        self.alpha_logscale = alpha_logscale
+        if self.alpha_logscale: # log scale alphas initialized to zeros
+            self.alpha = Parameter(torch.zeros(in_features) * alpha)
+            self.beta = Parameter(torch.zeros(in_features) * alpha)
+        else: # linear scale alphas initialized to ones
+            self.alpha = Parameter(torch.ones(in_features) * alpha)
+            self.beta = Parameter(torch.ones(in_features) * alpha)
+
+        self.alpha.requires_grad = alpha_trainable
+        self.beta.requires_grad = alpha_trainable
+
+        self.no_div_by_zero = 0.000000001
+
+    def forward(self, x):
+        '''
+        Forward pass of the function.
+        Applies the function to the input elementwise.
+        SnakeBeta ∶= x + 1/b * sin^2 (xa)
+        '''
+        alpha = self.alpha.unsqueeze(0).unsqueeze(-1) # line up with x to [B, C, T]
+        beta = self.beta.unsqueeze(0).unsqueeze(-1)
+        if self.alpha_logscale:
+            alpha = torch.exp(alpha)
+            beta = torch.exp(beta)
+        x = x + (1.0 / (beta + self.no_div_by_zero)) * pow(sin(x * alpha), 2)
+
+        return x
 
 class ResStack(nn.Module):
     def __init__(self, channel, num_layers=4):
@@ -20,10 +79,10 @@ class ResStack(nn.Module):
 
         self.blocks = nn.ModuleList([
             nn.Sequential(
-                nn.LeakyReLU(0.2),
+                SnakeBeta(channel),
                 nn.ReflectionPad1d(3**i),
                 nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=3, dilation=3**i)),
-                nn.LeakyReLU(0.2),
+                SnakeBeta(channel),
                 nn.utils.weight_norm(nn.Conv1d(channel, channel, kernel_size=1)),
             )
             for i in range(num_layers)
@@ -53,31 +112,32 @@ class Generator(nn.Module):
 
         self.generator = nn.Sequential(
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(mel_channel, 512, kernel_size=7, stride=1)),
+            nn.utils.weight_norm(nn.Conv1d(mel_channel, 256, kernel_size=7, stride=1)),
 
-            nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(512, 256, kernel_size=16, stride=8, padding=4)),
-
-            ResStack(256, num_layers=5),
-
-            nn.LeakyReLU(0.2),
+            SnakeBeta(256),
             nn.utils.weight_norm(nn.ConvTranspose1d(256, 128, kernel_size=16, stride=8, padding=4)),
 
-            ResStack(128, num_layers=7),
+            ResStack(128, num_layers=5),
 
-            nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(128, 64, kernel_size=4, stride=2, padding=1)),
+            SnakeBeta(128),
+            nn.utils.weight_norm(nn.ConvTranspose1d(128, 64, kernel_size=16, stride=8, padding=4)),
 
-            ResStack(64, num_layers=8),
+            ResStack(64, num_layers=7),
 
-            nn.LeakyReLU(0.2),
+            SnakeBeta(64),
             nn.utils.weight_norm(nn.ConvTranspose1d(64, 32, kernel_size=4, stride=2, padding=1)),
 
-            ResStack(32, num_layers=9),
+            ResStack(32, num_layers=8),
 
-            nn.LeakyReLU(0.2),
+            SnakeBeta(32),
+            nn.utils.weight_norm(nn.ConvTranspose1d(32, 16, kernel_size=4, stride=2, padding=1)),
+
+            ResStack(16, num_layers=9),
+
+            SnakeBeta(16),
+
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(32, 1, kernel_size=7, stride=1)),
+            nn.utils.weight_norm(nn.Conv1d(16, 1, kernel_size=7, stride=1)),
             nn.Tanh(),
         )
 
@@ -113,7 +173,7 @@ class Generator(nn.Module):
     @classmethod
     def from_config(cls, config: Dict[str, Any]) -> 'Generator':
         return Generator(mel_channels=config['audio']['n_mels'],
-                         **config['model'])
+                               **config['model'])
 
     @classmethod
     def from_checkpoint(cls, file: str) -> 'Generator':
