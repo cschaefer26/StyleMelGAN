de9a5923cd0d9f59379221718999e5188d06f203
cschafer26 <c.schaefer.home@gmail.com>
2023-05-23 08:25:02 +0200

    impl val loss.

--- stylemelgan/train.py
+++ stylemelgan/train.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     val_mel_files = mel_files[:512]
     train_mel_files = mel_files[512:]
     train_mel_dataloader = new_mel_dataloader(files=train_mel_files, segment_len=train_cfg['segment_len'],
-                                        hop_len=audio.hop_length, batch_size=train_cfg['batch_size'],
+                                        hop_len=audio.hop_length, batch_size=train_cfg['batch_size_mel'],
                                         num_workers=train_cfg['num_workers'])
     val_mel_dataloader = new_mel_dataloader(files=val_mel_files, segment_len=None,
                                         hop_len=audio.hop_length, batch_size=1,
@@ -96,84 +96,77 @@ if __name__ == '__main__':
     best_stft = 9999
     best_exp = 9999
 
-    ACC_ITER = 10
-
     for epoch in range(train_cfg['epochs']):
         pbar = tqdm.tqdm(enumerate(zip(dataloader, train_mel_dataloader), 1), total=len(dataloader))
         for i, (data, data_mel) in pbar:
+            step += 1
+            mel = data['mel'].to(device)
+            wav_real = data['wav'].to(device)
 
-            with torch.set_grad_enabled(True):
-                step += 1
-                mel = data['mel'].to(device)
-                wav_real = data['wav'].to(device)
+            wav_fake = g_model(mel)[:, :, :train_cfg['segment_len']]
 
-                wav_fake = g_model(mel)[:, :, :train_cfg['segment_len']]
+            d_loss = 0.0
+            g_loss = 0.0
+            stft_norm_loss = 0.0
+            stft_spec_loss = 0.0
 
-                d_loss = 0.0
-                g_loss = 0.0
-                stft_norm_loss = 0.0
-                stft_spec_loss = 0.0
+            if step > pretraining_steps:
+                # discriminator
+                d_fake = d_model(wav_fake.detach())
+                d_real = d_model(wav_real)
+                for (_, score_fake), (_, score_real) in zip(d_fake, d_real):
+                    d_loss += torch.mean(torch.sum(torch.pow(score_real - 1.0, 2), dim=[1, 2]))
+                    d_loss += torch.mean(torch.sum(torch.pow(score_fake, 2), dim=[1, 2]))
+                d_optim.zero_grad()
+                d_loss.backward()
+                d_optim.step()
 
-                if step > pretraining_steps:
-                    # discriminator
-                    d_fake = d_model(wav_fake.detach())
-                    d_real = d_model(wav_real)
-                    for (_, score_fake), (_, score_real) in zip(d_fake, d_real):
-                        d_loss += torch.mean(torch.sum(torch.pow(score_real - 1.0, 2), dim=[1, 2])) / ACC_ITER
-                        d_loss += torch.mean(torch.sum(torch.pow(score_fake, 2), dim=[1, 2])) / ACC_ITER
-
-                    d_loss.backward()
-
-                    if (i + 1) % ACC_ITER == 0:
-                        d_optim.step()
-                        d_optim.zero_grad()
-
-                    # generator
-                    d_fake = d_model(wav_fake)
-                    for (feat_fake, score_fake), (feat_real, _) in zip(d_fake, d_real):
-                        g_loss += torch.mean(torch.sum(torch.pow(score_fake - 1.0, 2), dim=[1, 2])) / ACC_ITER
-                        for feat_fake_i, feat_real_i in zip(feat_fake, feat_real):
-                            g_loss += 10. * F.l1_loss(feat_fake_i, feat_real_i.detach()) / ACC_ITER
+                # generator
+                d_fake = d_model(wav_fake)
+                for (feat_fake, score_fake), (feat_real, _) in zip(d_fake, d_real):
+                    g_loss += torch.mean(torch.sum(torch.pow(score_fake - 1.0, 2), dim=[1, 2]))
+                    for feat_fake_i, feat_real_i in zip(feat_fake, feat_real):
+                        g_loss += 10. * F.l1_loss(feat_fake_i, feat_real_i.detach())
 
 
-                mel_pred = data_mel['mel_post'].to(device)
+            mel_pred = data_mel['mel_post'].to(device)
 
-                wav_pred_fake = g_model(mel_pred)
-                mel_fake = mel_spectrogram(wav_pred_fake.squeeze(1), n_fft=1024, num_mels=80, sampling_rate=22050, hop_size=256,
-                                           win_size=1024, fmin=0, fmax=8000)
+            wav_pred_fake = g_model(mel_pred)
+            mel_fake = mel_spectrogram(wav_pred_fake.squeeze(1), n_fft=1024, num_mels=80, sampling_rate=22050, hop_size=256,
+                                       win_size=1024, fmin=0, fmax=8000)
+            #mel_pred_loss = 10000. * F.mse_loss(torch.exp(mel_fake), torch.exp(mel_pred))
+            #mel_pred_loss = 1000. * torch.norm(torch.exp(mel_fake) - torch.exp(mel_pred), p="fro") / torch.norm(torch.exp(mel_pred), p="fro")
+            diff = (torch.exp(mel_fake) - torch.exp(mel_pred)) ** 2
+            diff = diff.mean(1)
+            diff[diff < 0.005] = 0
+            mel_pred_loss = 100. * diff.sum()
 
-                diff = (torch.exp(mel_fake) - torch.exp(mel_pred)) ** 2
-                diff = diff.mean(1)
-                diff[diff < 0.005] = 0
-                mel_pred_loss = 100. * diff.sum() / ACC_ITER
 
-                print(mel_pred_loss)
+            print(mel_pred_loss)
 
-                factor = 1. if step < pretraining_steps else 0.
+            factor = 1. if step < pretraining_steps else 0.
 
-                stft_norm_loss, stft_spec_loss = multires_stft_loss(wav_fake.squeeze(1), wav_real.squeeze(1))
+            stft_norm_loss, stft_spec_loss = multires_stft_loss(wav_fake.squeeze(1), wav_real.squeeze(1))
+            g_loss_all = g_loss + mel_pred_loss + factor * (stft_norm_loss + stft_spec_loss)
 
-                g_loss_all = g_loss + mel_pred_loss + factor * (stft_norm_loss + stft_spec_loss) / ACC_ITER
-                g_loss_all.backward()
+            g_optim.zero_grad()
+            g_loss_all.backward()
+            g_optim.step()
 
-                if (i + 1) % ACC_ITER == 0:
-                    g_optim.step()
-                    g_optim.zero_grad()
+            pbar.set_description(desc=f'Epoch: {epoch} | Step {step} '
+                                      f'| g_loss: {g_loss:#.4} '
+                                      f'| d_loss: {d_loss:#.4} '
+                                      f'| stft_norm_loss {stft_norm_loss:#.4} '
+                                      f'| mel_pred_loss {mel_pred_loss:#.4} '
+                                      f'| stft_spec_loss {stft_spec_loss:#.4} ', refresh=True)
 
-                pbar.set_description(desc=f'Epoch: {epoch} | Step {step} '
-                                          f'| g_loss: {g_loss:#.4} '
-                                          f'| d_loss: {d_loss:#.4} '
-                                          f'| stft_norm_loss {stft_norm_loss:#.4} '
-                                          f'| mel_pred_loss {mel_pred_loss:#.4} '
-                                          f'| stft_spec_loss {stft_spec_loss:#.4} ', refresh=True)
+            summary_writer.add_scalar('generator_loss', g_loss, global_step=step)
+            summary_writer.add_scalar('generator_mel_pred_loss', mel_pred_loss, global_step=step)
+            summary_writer.add_scalar('stft_norm_loss', stft_norm_loss, global_step=step)
+            summary_writer.add_scalar('stft_spec_loss', stft_spec_loss, global_step=step)
+            summary_writer.add_scalar('discriminator_loss', d_loss, global_step=step)
 
-                summary_writer.add_scalar('generator_loss', g_loss, global_step=step)
-                summary_writer.add_scalar('generator_mel_pred_loss', mel_pred_loss, global_step=step)
-                summary_writer.add_scalar('stft_norm_loss', stft_norm_loss, global_step=step)
-                summary_writer.add_scalar('stft_spec_loss', stft_spec_loss, global_step=step)
-                summary_writer.add_scalar('discriminator_loss', d_loss, global_step=step)
-
-            if (step + 1) % train_cfg['eval_steps'] == 0:
+            if step % train_cfg['eval_steps'] == 0:
                 g_model.eval()
                 val_norm_loss = 0
                 val_spec_loss = 0
