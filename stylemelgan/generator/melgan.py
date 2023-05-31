a51bb5235d26f50843dad59e19b288ccc28ac2f0
cschafer26 <c.schaefer.home@gmail.com>
2023-05-31 07:08:23 +0200

    istft.

--- stylemelgan/generator/melgan.py
+++ stylemelgan/generator/melgan.py
@@ -111,12 +111,7 @@ class Generator(nn.Module):
 
         self.generator = nn.Sequential(
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(mel_channel, 512, kernel_size=7, stride=1)),
-
-            nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(512, 256, kernel_size=16, stride=8, padding=4)),
-
-            ResStackHifi(256, kernel_sizes=(3, 7, 11)),
+            nn.utils.weight_norm(nn.Conv1d(mel_channel, 256, kernel_size=7, stride=1)),
 
             nn.LeakyReLU(0.2),
             nn.utils.weight_norm(nn.ConvTranspose1d(256, 128, kernel_size=16, stride=8, padding=4)),
@@ -124,18 +119,23 @@ class Generator(nn.Module):
             ResStackHifi(128, kernel_sizes=(3, 7, 11)),
 
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.ConvTranspose1d(128, 64, kernel_size=4, stride=2, padding=1)),
+            nn.utils.weight_norm(nn.ConvTranspose1d(128, 64, kernel_size=16, stride=8, padding=4)),
 
-            ResStack(64, num_layers=8),
+            ResStackHifi(64, kernel_sizes=(3, 7, 11)),
 
             nn.LeakyReLU(0.2),
             nn.utils.weight_norm(nn.ConvTranspose1d(64, 32, kernel_size=4, stride=2, padding=1)),
 
-            ResStack(32, num_layers=9),
+            ResStack(32, num_layers=8),
+
+            nn.LeakyReLU(0.2),
+            nn.utils.weight_norm(nn.ConvTranspose1d(32, 16, kernel_size=4, stride=2, padding=1)),
+
+            ResStack(16, num_layers=9),
 
             nn.LeakyReLU(0.2),
             nn.ReflectionPad1d(3),
-            nn.utils.weight_norm(nn.Conv1d(32, 1, kernel_size=7, stride=1)),
+            nn.utils.weight_norm(nn.Conv1d(16, 1, kernel_size=7, stride=1)),
             nn.Tanh(),
 
         )
@@ -190,7 +190,7 @@ if __name__ == '__main__':
     x = torch.randn(3, 80, 1000)
     start = time.time()
     y  = model(x)
-    print(y.size())
+    #print(y.size())
 
     dur = time.time() - start
     print('dur ', dur)
