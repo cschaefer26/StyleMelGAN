8f1fd0db7e53489db832192a4be9b4ec99af2d9c
cschafer26 <c.schaefer.home@gmail.com>
2023-06-02 08:04:25 +0200

    diff.

--- stylemelgan/train.py
+++ stylemelgan/train.py
@@ -192,7 +192,12 @@ if __name__ == '__main__':
                         mel_fake = mel_spectrogram(wav_pred_fake.squeeze(1), n_fft=1024, num_mels=80, sampling_rate=22050, hop_size=256,
                                                    win_size=1024, fmin=0, fmax=8000)
                         #mel_pred_loss = 10000. * F.mse_loss(torch.exp(mel_fake), torch.exp(mel_pred))
-                        mel_pred_loss = 1000. * torch.norm(torch.exp(mel_fake) - torch.exp(val_mel_pred), p="fro") / torch.norm(torch.exp(val_mel_pred), p="fro")
+                        #mel_pred_loss = 1000. * torch.norm(torch.exp(mel_fake) - torch.exp(val_mel_pred), p="fro") / torch.norm(torch.exp(val_mel_pred), p="fro")
+                        diff = (torch.exp(mel_fake) - torch.exp(val_mel_pred)) ** 2
+                        diff = diff.mean(1)
+                        diff[diff < 0.005] = 0
+                        mel_pred_loss = 100. * diff.mean()
+
                         if mel_pred_loss > worst[0]:
                             worst = (mel_pred_loss, wav_pred_fake)
                         if mel_pred_loss < best[0]:
